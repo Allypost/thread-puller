5566e0e0b1a36f15028345f6fabcf9a5925ce8fd
Josip Igrec <me@allypost.net>
2018-07-07 19:31:09 +0200

    Donation link is now a separate entry

--- threadpuller2.js
+++ threadpuller2.js
@@ -137,7 +137,8 @@ const scripts = [
 ];
 
 const SETTINGS = `<img src="/images/cog.png" alt="Settings" id="settings" title="Settings" /><div id="settings-modal"><div class="settings-modal-content"></div></div><script>(function () { Settings && Settings.init && Settings.init() })();</script>`;
-const FOOTER = `<footer>Copyright &copy; ${new Date().getFullYear()} Allypost | All content is courtesy of <a href="https://www.4chan.org" target="_blank" rel="noopener noreferrer">4chan</a> | <a href="https://paypal.me/allypost" target="_blank" rel="noopener noreferrer">Donate to keep it going</a></footer>`;
+const DONATE = process.env.THREADPULLER_DONATE_LINK ? ` | <a href="${process.env.THREADPULLER_DONATE_LINK}" target="_blank" rel="noopener noreferrer">Donate if you like the sitedot</a>` : '';
+const FOOTER = `<footer>Copyright &copy; ${new Date().getFullYear()} Allypost | All content is courtesy of <a href="https://www.4chan.org" target="_blank" rel="noopener noreferrer">4chan</a>${DONATE}</footer>`;
 const META = `<meta charset="UTF-8"><meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge"><meta name="theme-color" content="#1E1E1E"><meta name="application-name" content="ThreadPuller - View 4chan thread images and videos"><meta name="msapplication-TileColor" content="#1E1E1E">`;
 // noinspection JSUnresolvedVariable
 const GoogleAnalytics = `<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.THREADPULLER_GA_KEY}"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','${process.env.THREADPULLER_GA_KEY}');</script>`;
