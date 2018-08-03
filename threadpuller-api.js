76664b5ec7ba976caa7cd14ca5ae90984e16ad4d
Josip Igrec <me@allypost.net>
2018-08-03 20:56:57 +0200

    Moved v1 API routes into separate file

--- threadpuller-api.js
+++ threadpuller-api.js
@@ -4,88 +4,29 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const Raven = require('raven');
 const SimpleLogger = require('./lib/Logging/SimpleLogger');
-const Entities = new (require('html-entities').AllHtmlEntities);
-const Fuse = require('fuse.js');
 
 require('dotenv-safe').load(
     {
         allowEmptyValues: true,
     });
 
-const redisConf = {
-    password: process.env.REDIS_PASSWORD,
-    prefix: 'ThreadPuller:',
-    db: process.env.REDIS_DB,
-};
-
-const htmlentities = Entities.encode;
-
-const redis = new (require('./lib/DB/Redis'))(redisConf).redis;
-
-const Boards = new (require('./lib/API/Boards'))(redis);
-const Threads = new (require('./lib/API/Threads'))(redis);
-const Posts = new (require('./lib/API/Posts'))(redis);
-const API = new (require('./lib/API/API'))(`${process.env.THREADPULLER_DOMAIN_API}/v1`, 'v1');
+const redis = new (require('./lib/DB/Redis'))(
+    {
+        password: process.env.REDIS_PASSWORD,
+        prefix: 'ThreadPuller:',
+        db: process.env.REDIS_DB,
+    },
+).redis;
 
 const app = express();
-const Router = express.Router({});
+const Router = (require('./lib/Router/API/v1'))(redis);
 
 Router.use(cookieParser());
 Router.use(helmet());
-
 Raven.config(process.env.SENTRY_DSN_URL).install();
 
-Router.get('/boards', async (req, res) => {
-    const boards = await Boards.get();
-
-    return res.json(API.embedApiLinks(boards));
-});
-
-Router.get('/boards/:board', async (req, res) => {
-    const board = htmlentities(req.params.board);
-    const boardPosts = await Threads.get(board);
-
-    res.json(API.embedApiLinks(boardPosts, board));
-});
-
-Router.get('/boards/:board/search/:query([a-zA-Z0-9_ %]{2,})', async (req, res) => {
-    const board = htmlentities(req.params.board);
-    const searchOptions = {
-        caseSensitive: true,
-        shouldSort: true,
-        tokenize: false,
-        threshold: 0.3,
-        location: 0,
-        distance: 20,
-        maxPatternLength: 32,
-        minMatchCharLength: 2,
-        keys: [
-            {
-                name: 'body.title',
-                weight: 0.7,
-            },
-            {
-                name: 'body.content',
-                weight: 0.3,
-            },
-        ],
-    };
-    const rawPosts = await Threads.get(board) || [];
-    const fuse = new Fuse(rawPosts, searchOptions);
-    const query = String(req.params.query || req.query.q || '');
-    const posts = fuse.search(query);
-
-    res.json(API.embedApiLinks(posts, board));
-});
-
-Router.get('/boards/:board/thread/:thread', async (req, res) => {
-    const p = Object.entries(req.params)
-                    .map(([ key, value ]) => [ key, htmlentities(value) ])
-                    .reduce((obj, [ k, v ]) => Object.assign(obj, { [ k ]: v }), {});
-
-    const posts = await Posts.get(p.board, p.thread);
-
-    res.json(API.embedApiLinks(posts, p.board, p.thread));
+app.get('/', (req, res) =>  {
+    return res.redirect('v1/');
 });
 
 app.use('/v1/', Router);
