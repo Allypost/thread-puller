cb9676da59e590cacc275b7e549350d9c6dd4cea
Josip Igrec <me@allypost.net>
2018-10-08 22:24:04 +0200

    Added pm2 config

---
+++ threadpuller-presence.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /**
+     * Application configuration section
+     * http://pm2.keymetrics.io/docs/usage/application-declaration/
+     */
+    apps: [
+
+        {
+            name: 'ThreadPuller Presence',
+            script: './threadpuller-presence.js',
+            watch: [
+                './threadpuller-presence.js',
+                './lib/',
+                './.env'
+            ],
+            cwd: __dirname,
+        },
+    ],
+};
