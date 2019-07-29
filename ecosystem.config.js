b00fb5c6ae4f8ac86deb89d517a897d493ac283b
Josip Igrec <me@allypost.net>
2019-07-29 23:30:19 +0200

    Fresh start

--- ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-const web = require('./threadpuller-web.config').apps.pop();
-const api = require('./threadpuller-api.config').apps.pop();
-const presence = require('./threadpuller-presence.config').apps.pop();
-
-module.exports = {
-    apps: [ web, api, presence ],
-};
