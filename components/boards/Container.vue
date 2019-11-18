1bddabd70a903762368f43c61858eccc11e7bcc6
Josip Igrec <me@allypost.net>
2019-11-18 06:37:19 +0100

    Implement search components in board and thread list

--- components/boards/Container.vue
+++ components/boards/Container.vue
@@ -7,28 +7,67 @@
 </style>
 
 <template>
-    <div class="container">
-        <board-entry
-            v-for="board of boards"
-            :key="board.board"
-            :board="board"
+    <div>
+        <SearchInput
+            :data="rawBoards"
+            :keys="keys"
+            @updateData="filteredBoards = arguments[0]"
+            @updateQuery="updateQuery"
         />
+        <div class="container">
+            <board-entry
+                v-for="board of boards"
+                :key="board.board"
+                :board="board"
+            />
+        </div>
     </div>
 </template>
 
 <script>
     import { mapGetters } from 'vuex';
+    import SearchInput from '../SearchInput';
     import BoardEntry from './Board';
 
     export default {
         name: 'Container',
 
-        components: { BoardEntry },
+        components: { SearchInput, BoardEntry },
+
+        data() {
+            return {
+                filteredBoards: null,
+                keys: [
+                    {
+                        name: 'description',
+                        weight: 0.50,
+                    },
+                    {
+                        name: 'title',
+                        weight: 0.40,
+                    },
+                    {
+                        name: 'board',
+                        weight: 0.10,
+                    },
+                ],
+            };
+        },
 
         computed: {
+            boards() {
+                return this.filteredBoards || this.rawBoards;
+            },
+
             ...mapGetters({
-                'boards': 'boards/get',
+                'rawBoards': 'boards/get',
             }),
+        },
+
+        methods: {
+            updateQuery(action, data) {
+                this.$router[ action ](data);
+            },
         },
     };
 </script>
--- components/threads/Container.vue
+++ components/threads/Container.vue
@@ -7,28 +7,67 @@
 </style>
 
 <template>
-    <div class="container">
-        <board-thread
-            v-for="thread in threads"
-            :key="thread.id"
-            :thread="thread"
+    <div>
+        <SearchInput
+            :data="rawThreads"
+            :keys="keys"
+            @updateData="filteredThreads = arguments[0]"
+            @updateQuery="updateQuery"
         />
+        <div class="container">
+            <board-thread
+                v-for="thread in threads"
+                :key="thread.id"
+                :thread="thread"
+            />
+        </div>
     </div>
 </template>
 
 <script>
     import { mapGetters } from 'vuex';
+    import SearchInput from '../SearchInput';
     import BoardThread from './Thread';
 
     export default {
         name: 'ThreadsContainer',
 
-        components: { BoardThread },
+        components: { SearchInput, BoardThread },
+
+        data() {
+            return {
+                filteredThreads: null,
+                keys: [
+                    {
+                        name: 'body.title',
+                        weight: 0.50,
+                    },
+                    {
+                        name: 'body.content',
+                        weight: 0.40,
+                    },
+                    {
+                        name: 'file.name',
+                        weight: 0.10,
+                    },
+                ],
+            };
+        },
 
         computed: {
+            threads() {
+                return this.filteredThreads || this.rawThreads;
+            },
+
             ...mapGetters({
-                'threads': 'threads/get',
+                'rawThreads': 'threads/get',
             }),
+        },
+
+        methods: {
+            updateQuery(action, data) {
+                this.$router[ action ](data);
+            },
         },
     };
 </script>
