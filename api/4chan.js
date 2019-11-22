d58731cde56abc65d0b42e5545f6b6ed1b4f720c
Josip Igrec <me@allypost.net>
2019-11-22 15:21:22 +0100

    Add basic search page

--- api/4chan.js
+++ api/4chan.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 import express from 'express';
+import bodyParser from 'body-parser';
 import consola from 'consola';
 import Boards from '../lib/Fetchers/4chan/Boards';
 import Posts from '../lib/Fetchers/4chan/Posts';
 import Threads from '../lib/Fetchers/4chan/Threads';
+import PostInfo from '../lib/Formatters/PostInfo';
+import { Fuse } from '../lib/Search/Fuse';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 function fetchData(cb) {
     return async (req, res) => {
@@ -22,6 +28,157 @@ function fetchData(cb) {
             });
         }
     };
+}
+
+/**
+ * @param {String} query
+ * @param {String} [board]
+ * @param {Boolean} [allowNSFW]
+ * @returns {AsyncIterableIterator<{total: Number, results: Number, threads: Object[]}>}
+ */
+async function* search4ChanBoardGenerator(query, { board = '' }) {
+    let offset = 0;
+
+    const threadList = new Map();
+    const baseURL = new URL('https://find.4chan.org/api');
+    const opts = new URLSearchParams({
+        'q': query,
+        'b': board,
+        'o': offset,
+    });
+    baseURL.search = opts.toString();
+
+    async function doFetch() {
+        const response = await axios.get(baseURL.toString(), {
+            responseType: 'application/json',
+        });
+
+        const { nhits: total, threads: threads = [] } = response.data;
+
+        const hasUnique =
+                  threads
+                      .map((thread) => threadList.has(thread.thread))
+                      .includes(false);
+
+        if (hasUnique) {
+            return { threads, total };
+        } else {
+            return null;
+        }
+    }
+
+    let results = 0;
+    while (true) {
+        const data = await doFetch();
+
+        if (!data) {
+            break;
+        }
+
+        const { threads, total } = data;
+
+        results += threads.reduce((acc, { posts }) => acc + posts.length, 0);
+
+        yield {
+            total,
+            results,
+            threads:
+                threads
+                    .filter((thread) => !threadList.has(thread.thread))
+                    .map(({ board, posts: [ post ] }) => ({ item: PostInfo.parse(board, post), score: 1 })),
+        };
+
+        for (const thread of threads) {
+            threadList.set(thread.thread, thread);
+        }
+
+        offset += 10;
+        opts.set('o', String(offset));
+        baseURL.search = opts.toString();
+    }
+}
+
+/**
+ * @param {String} query
+ * @param {String} [board]
+ * @param {Boolean} [allowNSFW]
+ * @returns {AsyncIterableIterator<{total: Number, results: Number, threads: Object[]}>}
+ */
+async function* searchBoardGenerator(query, { board = '', allowNSFW = true }) {
+    async function searchThreads(board) {
+        const searchKeys = [
+            {
+                name: 'body.title',
+                weight: 0.50,
+            },
+            {
+                name: 'body.content',
+                weight: 0.40,
+            },
+            {
+                name: 'file.name',
+                weight: 0.10,
+            },
+        ];
+
+        const allThreads = await Threads.get(board);
+
+        return (
+            Fuse(allThreads, searchKeys)
+                .search(query)
+        );
+    }
+
+    /**
+     * @param {Boolean} allowNSFW
+     * @returns {Promise<Object[]>}
+     */
+    async function getBoards(allowNSFW = true) {
+        const boards = await Boards.get();
+
+        if (allowNSFW) {
+            return boards;
+        }
+
+        return boards.filter(({ nsfw }) => !nsfw);
+    }
+
+    /**
+     * @returns {Promise<{total: Number, results: Number, threads: Object[]}>}
+     */
+    async function one(board) {
+        const threads = await searchThreads(board);
+
+        return {
+            total: 1,
+            results: 1,
+            threads,
+        };
+    }
+
+    /**
+     * @returns {Promise<{total: Number, results: Number, threads: Object[]}>}
+     */
+    async function* all() {
+        const boards = await getBoards(allowNSFW);
+
+        let results = 0;
+        for (const { board } of boards) {
+            const threads = await searchThreads(board);
+
+            yield {
+                total: boards.length,
+                results: ++results,
+                threads,
+            };
+        }
+    }
+
+    if (board) {
+        yield one(board);
+    } else {
+        yield* all();
+    }
 }
 
 app.get('/boards', fetchData(async () => {
@@ -45,6 +202,60 @@ app.get('/boards/:board/thread/:thread', fetchData(async ({ params }) => {
 
     return await Posts.get(board, thread);
 }));
+
+app.post('/search', async ({ body }, res) => {
+    const { query, nsfw: allowNSFW, board = '' } = body;
+
+    if (!query) {
+        return res.json([]);
+    }
+
+    const resultGenerator = search4ChanBoardGenerator(query, { board, allowNSFW });
+
+    res.set('Content-Type', 'application/stream+json');
+    res.set('Transfer-Encoding', 'chunked');
+
+    try {
+        let result = await resultGenerator.next();
+        while (!result.done) {
+            res.write(JSON.stringify(result.value));
+            res.write('\n');
+
+            result = await resultGenerator.next();
+        }
+    } catch (e) {
+        consola.log(e);
+    }
+
+    res.send();
+});
+
+app.post('/search/local', async ({ body }, res) => {
+    const { query, nsfw: allowNSFW, board = '' } = body;
+
+    if (!query) {
+        return res.json([]);
+    }
+
+    const resultGenerator = searchBoardGenerator(query, { board, allowNSFW });
+
+    res.set('Content-Type', 'application/stream+json');
+    res.set('Transfer-Encoding', 'chunked');
+
+    try {
+        let result = await resultGenerator.next();
+        while (!result.done) {
+            res.write(JSON.stringify(result.value));
+            res.write('\n');
+
+            result = await resultGenerator.next();
+        }
+    } catch (e) {
+        consola.log(e);
+    }
+
+    res.send();
+});
 
 module.exports = {
     path: '/api/',
---
+++ pages/search.vue
@@ -0,0 +1,242 @@
+<style scoped lang="scss">
+    @import "../assets/style/modules/include";
+
+    .container {
+        @extend %container-base;
+    }
+
+    %text-shadow {
+        text-shadow: 1px 1px 3px #000000, 0 0 5px #000000, 3px 3px 8px #000000;
+    }
+
+    $header-font-size: 3em;
+
+    h1, h2 {
+        @extend %text-shadow;
+        @include no-select();
+    }
+
+    h1 {
+        text-align: center;
+        font-size: $header-font-size;
+        margin: .67em 0;
+        margin-bottom: 0;
+
+        > a {
+            text-shadow: none;
+        }
+    }
+
+    h2 {
+        font-size: #{$header-font-size * .8};
+        margin-top: 0;
+        color: #ffffff;
+    }
+</style>
+
+<template>
+    <div>
+        <h1>
+            Search threads
+        </h1>
+        <ThreadBacklinks
+            :back-link="{ name: 'Boards' }"
+            :external-href="externalHref"
+        />
+        <br>
+        <form @submit.prevent="doSearch">
+            <label>
+                Query:
+                <input
+                    v-model="form.query"
+                    type="text"
+                    :disabled="loading"
+                >
+            </label>
+            <label>
+                Board:
+                <select
+                    v-model="form.board"
+                    :disabled="loading"
+                >
+                    <option
+                        v-for="b in shownBoards"
+                        :key="b.board"
+                        :value="b.board"
+                    >{{ b.board }} - {{ b.title }}
+                    </option>
+                    <option value="">ALL</option>
+                </select>
+            </label>
+            <label>
+                Show NSFW:
+                <input
+                    v-model="form.nsfw"
+                    type="checkbox"
+                >
+            </label>
+            <input
+                type="submit"
+                value="Search"
+                :disabled="loading"
+                @click.prevent="doSearch"
+            >
+        </form>
+        <LoaderSpinner
+            v-if="loading"
+            size="20px"
+        />
+        <progress
+            v-if="loading"
+            :value="progress.value"
+            :max="progress.max"
+        />
+        <LoaderSpinner
+            v-if="loading"
+            size="20px"
+        />
+        <div class="container">
+            <BoardThread
+                v-for="thread in results"
+                :key="thread.item.id"
+                :thread="thread.item"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import ThreadBacklinks from '../components/ThreadBacklinks';
+    import BoardThread from '../components/threads/Thread';
+    import PepeImage from '../assets/images/pepe.png';
+    import LoaderSpinner from '../components/threads/thread/media/LoaderSpinner';
+
+    function e(name, content) {
+        return { hid: name, name, content };
+    }
+
+    export default {
+        name: 'Search',
+
+        components: { ThreadBacklinks, BoardThread, LoaderSpinner },
+
+        head() {
+            return {
+                title: 'Search',
+                meta: [
+                    e('og:title', 'Search | ThreadPuller'),
+                    e('og:description', 'Search the current boards.'),
+                    e('description', 'Search the current boards.'),
+                    e('og:image', PepeImage),
+                ],
+            };
+        },
+
+        data() {
+            return {
+                form: {
+                    board: '',
+                    query: '',
+                    nsfw: true,
+                },
+                progress: {
+                    value: 0,
+                    max: 0,
+                },
+                loading: false,
+                results: [],
+            };
+        },
+
+        computed: {
+            shownBoards() {
+                if (this.form.nsfw) {
+                    return this.boards;
+                }
+
+                return this.boards.filter(({ nsfw }) => !nsfw);
+            },
+
+            externalHref() {
+                return `https://find.4channel.org/?q=${ encodeURIComponent(this.form.query) }`;
+            },
+
+            ...mapGetters({
+                'boards': 'boards/get',
+            }),
+        },
+
+        async fetch({ store }) {
+            const isServer = process.server;
+
+            await store.dispatch('boards/fetch', { isServer });
+        },
+
+        methods: {
+            async * makeTextFileLineIterator(response) {
+                const utf8Decoder = new TextDecoder('utf-8');
+                const reader = response.body.getReader();
+                let { value: chunk, done: readerDone } = await reader.read();
+                chunk = chunk ? utf8Decoder.decode(chunk) : '';
+
+                const re = /\n|\r|\r\n/gm;
+                let startIndex = 0;
+                let result;
+
+                for (; ;) {
+                    result = re.exec(chunk);
+                    if (!result) {
+                        if (readerDone) {
+                            break;
+                        }
+                        const remainder = chunk.substr(startIndex);
+                        ({ value: chunk, done: readerDone } = await reader.read());
+                        chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+                        re.lastIndex = 0;
+                        startIndex = 0;
+                        continue;
+                    }
+                    yield chunk.substring(startIndex, result.index);
+                    startIndex = re.lastIndex;
+                }
+                if (startIndex < chunk.length) {
+                    // last line didn't end in a newline char
+                    yield chunk.substr(startIndex);
+                }
+            },
+
+            async doSearch() {
+                if (this.loading) {
+                    return;
+                }
+
+                this.loading = true;
+                this.$set(this, 'results', []);
+
+                const result = await fetch('/api/search/local', {
+                    method: 'POST',
+                    body: JSON.stringify(this.form),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                for await(const line of this.makeTextFileLineIterator(result)) {
+                    const { threads, results, total } = JSON.parse(line);
+
+                    this.progress.value = results;
+                    this.progress.max = total;
+
+                    const newResults = [ ...this.results, ...threads ].sort(({ score: a }, { score: b }) => a - b);
+
+                    this.$set(this, 'results', newResults);
+                }
+
+                this.loading = false;
+                this.progress.value = 0;
+                this.progress.max = 1;
+            },
+        },
+    };
+</script>
