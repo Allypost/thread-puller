8730e48127c77805138cc7ed1501aee53cfd3e6d
Josip Igrec <me@allypost.net>
2020-04-19 15:09:30 +0200

    Update 4chan refresher to be more sane

--- modules/4chan/refresher.js
+++ modules/4chan/refresher.js
@@ -1,5 +1,24 @@
-import { refreshers } from '../../lib/Refreshers/4chan/Refresher';
+import consola from 'consola';
+import { clean, populate, processOne } from '../../lib/Refreshers/4chan/Refresher';
 
 export default function refresher() {
-    refreshers.forEach((refresher) => refresher());
+    let intervalId = null;
+
+    const info = (message) => consola.info({ message, badge: true });
+
+    this.nuxt.hook('listen', async () => {
+        info('Cleaning old refresher data...');
+        await clean();
+        info('Populating refresher...');
+        await populate();
+        info('Starting refresher listener...');
+        intervalId = setInterval(() => processOne(), 2500);
+        info('Refresher started');
+    });
+
+    this.nuxt.hook('close', async () => {
+        info('Shutting down refresher listener...');
+        clearInterval(intervalId);
+        info('Refresher shut down');
+    });
 }
